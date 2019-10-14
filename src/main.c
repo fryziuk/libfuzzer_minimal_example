4dbd7a21612c81f70170a1aa32dd25b33ebfe616
Oleksandr Fryziuk <oleksandrfryziuk@Oleksandrs-MacBook-Pro.local>
2019-10-14 14:58:18 +0300

    Minimal libfuzzer example.
    Used to reproduce lldb issue inside clion  (MacOS Catalina + lldb clang)
    set C Compiller to /usr/local/opt/llvm/bin/clang

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    // try start debug this from macOS Clion, doesn't work
+    // lldb from terminal works ok
+    return 0;
+}
